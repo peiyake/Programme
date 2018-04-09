be45373d62463ad54a7c26d50fe4ff5264d9a39e
piak <piak@piakdeMacBook-Pro.local>
2018-04-09 11:10:21 +0800

    增加组播编程实例

---
+++ broadcast/broadcastsrv.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <sys/types.h>	       /* See NOTES */
+#include <sys/socket.h>
+#include <linux/in.h>
+#include <string.h>
+#include <errno.h>
+//#include <arpa/inet.h>
+	
+int sockfd;
+
+#define UDP_PORT 5008
+#define BROADCAST_IP 	"224.1.1.1"
+#define INTERFACE_IP 	"10.60.0.68"
+int create_socket(void)
+{
+#ifdef SERVER
+	sockfd = socket(AF_INET,SOCK_DGRAM,0);
+	if(-1 == sockfd)
+	{
+		fprintf(stderr,"sockfd err:%s\n",strerror(errno));
+		return -1;
+	}
+#else
+	sockfd = socket(AF_INET,SOCK_DGRAM,0);
+	if(-1 == sockfd)
+	{
+		fprintf(stderr,"sockfd err:%s\n",strerror(errno));
+		return -1;
+	}
+#endif
+	return 0;
+}
+int main(int argc, const char *argv[])
+{
+	int ret = -1;
+	unsigned int addrlen;
+	char recvbuf[1024];
+	struct sockaddr_in sockaddr,peeraddr;
+	ret = create_socket();
+	if(-1 == ret)
+	{
+		fprintf(stderr,"create_socket failed\n");
+		return -1;	
+	}
+	memset(&sockaddr,0,sizeof(sockaddr));
+	sockaddr.sin_family = AF_INET;
+	sockaddr.sin_addr.s_addr = INADDR_ANY;
+	sockaddr.sin_port = htons(UDP_PORT);
+#ifdef SERVER
+	ret = bind(sockfd,(struct sockaddr *)&sockaddr,sizeof(sockaddr));	
+	if(-1 == ret)
+	{
+		fprintf(stderr,"bind err:%s\n",strerror(errno));
+		close(sockfd);
+		return -1;
+	}
+#if 1
+	struct ip_mreq mreq;
+	inet_pton(AF_INET,BROADCAST_IP,&(mreq.imr_multiaddr.s_addr));
+	inet_pton(AF_INET,INTERFACE_IP,&(mreq.imr_interface.s_addr));
+//	mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+
+	int loop = 1;
+	ret = setsockopt(sockfd,IPPROTO_IP,IP_MULTICAST_LOOP,&loop,sizeof(loop));
+	if(-1 == ret)
+	{
+		fprintf(stderr,"setsockopt set loop err:%s\n",strerror(errno));
+	}
+
+	ret = setsockopt(sockfd,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq));
+	if(-1 == ret)
+	{
+		fprintf(stderr,"setsockopt set cast err:%s\n",strerror(errno));
+	}
+#endif
+	for(;;)
+	{
+		memset(&peeraddr,0,sizeof(peeraddr));
+		addrlen = sizeof(peeraddr);
+		memset(recvbuf,0,sizeof(recvbuf));
+		ret = recvfrom(sockfd,recvbuf,sizeof(recvbuf),0,(struct sockaddr *)&peeraddr,(socklen_t*)&addrlen);
+		if(-1 == ret)
+		{
+			fprintf(stderr,"recvfrom err:%s\n",strerror(errno));
+			continue;
+		}
+		fprintf(stdout,"recvmsg:\t%s\n",recvbuf);
+		if(!strcmp(recvbuf,"quit"))
+		{
+			fprintf(stdout,"quit...\n");
+			break;
+		}
+	}
+#else
+	for(;;)
+	{
+		memset(&peeraddr,0,sizeof(peeraddr));
+		addrlen = sizeof(peeraddr);
+		memset(recvbuf,0,sizeof(recvbuf));
+		if(NULL != fgets(recvbuf,sizeof(recvbuf),stdin))
+		{
+			ret = sendto(sockfd,recvbuf,strlen(recvbuf),0,(struct sockaddr*)&sockaddr,addrlen);
+			fprintf(stdout,"sendto %d bytes \n",ret);
+		}
+		if(!strcmp(recvbuf,"quit"))
+		{
+			fprintf(stdout,"quit...\n");
+			break;
+		}
+	}
+#endif
+	close(sockfd);
+	return 0;
+}
